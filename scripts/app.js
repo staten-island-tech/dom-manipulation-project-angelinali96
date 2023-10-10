464bbdc7c9e4fe9b8f9f284e1071bce050740cc9
angelinali96 <136626552+angelinali96@users.noreply.github.com>
2023-10-10 10:20:34 -0400

    init

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+/*
+Using JavaScript you will allow users to input information into a form. The
+form will then push data from a JavaScript object into the HTML through
+DOM manipulation. The project must contain the following
+-Create an object called “DOMSelectors” to hold your DOM Selectors
+-Create a function that creates an object and calls the following functions
+-Create a function that injects the newly created object into the DOM
+-Create a function that clears the input fields after injecting the object
+-Create a function to remove an object after they have been created
+*/
