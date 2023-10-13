f0c2a8b5e14a10a281049821f516cff0953dec40
angelinali96 <136626552+angelinali96@users.noreply.github.com>
2023-10-13 10:11:45 -0400

    ???

--- scripts/app.js
+++ scripts/app.js
@@ -9,20 +9,24 @@ DOM manipulation. The project must contain the following
 -Create a function to remove an object after they have been created
 */
 const DOMSelectors = {
+    form: document.querySelector("#form"),
     button : document.getElementById("btn"), 
+    delete : document.getElementById("del"),
     input1 : document.getElementById("input1"),
     input2 : document.getElementById("input2"),
     text : document.querySelector("p"),
 };
-function synth(inputs){
-    DOMSelectors.text.insertAdjacentHTML("afterend", `<strong>Your new favorite color is ${DOMSelectors.input1.value} ${inputs.item2}~</strong>`)
+function synth(DOMSelectors){
+    DOMSelectors.text.insertAdjacentHTML("afterend", `<p>Your new favorite color is ${DOMSelectors.input1.value} ${DOMSelectors.input2.value}~</p>`)
 }
-const inputs = {
-    item1: DOMSelectors.input1.value,
-    item2: DOMSelectors.input2.value
-};
-DOMSelectors.button.addEventListener("click", function(){
-        synth(inputs);
-        console.log(DOMSelectors.input1.value);
+DOMSelectors.form.addEventListener("submit", function(event){
+        event.preventDefault();
+        synth(DOMSelectors);
+        DOMSelectors.input1.value = "";
+        DOMSelectors.input2.value = "";
     }
+);
+DOMSelectors.delete.addEventListener("click", function(){
+
+}
 );
