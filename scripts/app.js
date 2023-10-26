5e0b2a132de19bbb66ce856e5d707ca693612ef6
angelinali96 <136626552+angelinali96@users.noreply.github.com>
2023-10-26 09:57:52 -0400

    ????????????

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-const DOMSelectors = {
+const DOMSelectors = { //array of domselectors to call
     form: document.querySelector("#form"),
     button : document.getElementById("btn"), 
     delete : document.querySelectorAll(".del"),
@@ -8,28 +8,28 @@ const DOMSelectors = {
     text : document.querySelector("p"),
     res : document.querySelector(".result")
   };
-  const newColor = {
+  const newColor = { //inputs
     input1: DOMSelectors.input1,
     input2: DOMSelectors.input2,
     input3: DOMSelectors.input3
   };
   function addRes(DOMSelectors){
-    const one = hex2rgb(newColor.input1.value);
+    const one = hex2rgb(newColor.input1.value); //retrieves rgb color code from hex inputs
     const two = hex2rgb(newColor.input2.value);
     const three = hex2rgb(newColor.input3.value);
-    const mix1 = Math.round((one[0] + two[0] + three[0])/3);
+    const mix1 = Math.round((one[0] + two[0] + three[0])/3); //takes average of rgb for respective position & rounds to nearest int
     const mix2 = Math.round((one[1] + two[1] + three[1])/3);
     const mix3 = Math.round((one[2] + two[2] + three[2])/3);
-    const resColor = `rgba(${mix1},${mix2},${mix3})`;
+    const resColor = `rgba(${mix1},${mix2},${mix3})`; //averaged color
     DOMSelectors.res.insertAdjacentHTML("afterbegin", `<div class="card" style="background-color: ${resColor}">
     <p>Your new favorite color is ${resColor}~</p>
     <button class="del">remove</button>
-    </div>`)
+    </div>`) //insert html with div color equal to output
   }
   DOMSelectors.form.addEventListener("submit", function(event){
-        event.preventDefault();
+        event.preventDefault(); //prevent reload on submit
         addRes(DOMSelectors);
-        DOMSelectors.input1.value = "";
+        DOMSelectors.input1.value = ""; //clear inputs
         DOMSelectors.input2.value = "";
         DOMSelectors.input3.value = "";
         removeRes();
@@ -37,14 +37,14 @@ const DOMSelectors = {
   );
   function removeRes(){
     const delButton = document.querySelectorAll(".del");
-    delButton.forEach(
+    delButton.forEach( //for each delete button generated, event listener of remove is appended
         (del) => del.addEventListener("click", function(){
             del.parentElement.remove();
             //del.parentElement.style.display = none;
         })
     );
   }
-  function hex2rgb(hex){
+  function hex2rgb(hex){ //converts hex code to rgb by slicing into xx xx xx and parsing hex integer
     const r = parseInt(hex.slice(1,3),16);
     const g = parseInt(hex.slice(3,5),16);
     const b = parseInt(hex.slice(5,7),16);
